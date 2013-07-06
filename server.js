17718182e5ba95a909cfd2f69c5971753e78f1af
Sameer Karim <sameerkarim1@gmail.com>
2013-07-06 11:07:04 +0800

    Added server conf file which is used by upstart for the start server and stop server commands. Also modified server.js so it uses port 80 instead of 3000.

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ server.on('request', function (req, res) {
 	res.writeHead('200', {'Content-type': 'text/plain'});
 	res.end('Hello World \n');
 });
-server.listen(3000);
-console.log('Server up and running on http://198.199.96.186:3000/ or http://bizzee-bee.com:3000/');
+server.listen(80);
+console.log('Server up and running on http://198.199.96.186/ or http://bizzee-bee.com/');
